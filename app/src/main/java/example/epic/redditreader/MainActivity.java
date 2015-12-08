3ef0ef19b862de3b99faf494a9d478e215eafbd3
Guest User <student@epicodus.com>
2015-12-08 10:17:30 -0800

    needs some debugging

--- app/src/main/java/example/epic/redditreader/MainActivity.java
+++ app/src/main/java/example/epic/redditreader/MainActivity.java
@@ -1,13 +1,26 @@
 package example.epic.redditreader;
 
+import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import example.epic.redditreader.ui.PostsFragment;
+
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addFragment();
+    }
+
+    void addFragment(){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragments_holder
+                        , PostsFragment.newInstance("askreddit"))
+                .commit();
     }
 }
